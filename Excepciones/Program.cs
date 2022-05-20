2f5301a139534648a1d269fc81e024743bd78df9
SystemsMiguel <105815147+SystemsMiguel@users.noreply.github.com>
2022-05-20 15:17:47 -0700

    create

---
+++ Excepciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese un numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese otro numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadKey();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
